2066fb19d873732243bc2e9ba2097457e1358f22
Luis Rodriguez <luis.rodriguezgil@deusto.es>
2013-09-10 16:38:22 +0200

    Added FastTemplate class

---
+++ FastTemplate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleDeustoTwo
+{
+
+    /// <summary>
+    /// Small template engine intended to be extremely simplistic.
+    /// </summary>
+    public class FastTemplate
+    {
+
+        /// <summary>
+        /// Renders the template using the provided variables.
+        /// </summary>
+        /// <param name="template"></param>
+        /// <param name="variables"></param>
+        /// <returns></returns>
+        public string Replace(string template, Dictionary<string, string> variables)
+        {
+            foreach (KeyValuePair<string, string> entry in variables)
+                template = template.Replace("{{" + entry.Key + "}}", entry.Value);
+            return template;
+        }
+    }
+}
